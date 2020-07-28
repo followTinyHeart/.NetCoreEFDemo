0a28b533c6dc0a7731319ddf3a6cb846fa0ef5ea
skY <1530321852@qq.com>
2020-07-28 15:22:05 +0800

    First Commit

---
+++ ContosoUniversity/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ContosoUniversity.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public String Guid { get; set; }
+        public String LastName { get; set; }
+        public String FirstName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+        public Boolean IsDelete { get; set; }
+        public DateTime CreateTime { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
